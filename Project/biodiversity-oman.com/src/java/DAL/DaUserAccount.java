289265300b9dc983cf3d325de0f947206bcf0b9c
FriiZzLe <lenny.donnez@gmail.com>
2015-02-13 23:43:23 +0100

    DataSource klasse toegevoegd OPGELET
    Ik gebruik geen usbw dus credentials zijn voor jullie anders!

--- Project/biodiversity-oman.com/src/java/DAL/DaUserAccount.java
+++ Project/biodiversity-oman.com/src/java/DAL/DaUserAccount.java
@@ -11,4 +11,5 @@ package DAL;
  */
 public class DaUserAccount {
 	
+	
 }
---
+++ Project/biodiversity-oman.com/src/java/DAL/DataSource.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author lennyasus
+ */
+public class DataSource {
+	
+	public static java.sql.Connection getConnection() throws SQLException {
+        	return DriverManager.getConnection("jdbc:mysql://localhost:3306/omandb", "root", "root");
+    }
+}
