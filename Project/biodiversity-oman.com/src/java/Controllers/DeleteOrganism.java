eb2866423072f3f685e2f0fe3dad270ae149ab7b
Lenny <lenny.donnez@gmail.com>
2015-02-27 21:30:51 +0100

    Revert ad80872..ee36894
    This rolls back to commit ad8087290b71667d19174ab77da2c587761d62ef.

--- Project/biodiversity-oman.com/src/java/Controllers/DeleteOrganism.java
+++ Project/biodiversity-oman.com/src/java/Controllers/DeleteOrganism.java
@@ -5,8 +5,11 @@
  */
 package Controllers;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import Service.*;
+import java.io.*;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,10 +18,10 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author bert
+ * @author Tom
  */
-@WebServlet(name = "InsertOrganism", urlPatterns = {"/InsertOrganism"})
-public class InsertOrganism extends HttpServlet {
+@WebServlet(name = "DeleteOrganism", urlPatterns = {"/DeleteOrganism"})
+public class DeleteOrganism extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -33,17 +36,12 @@ public class InsertOrganism extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet InsertOrganism</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet InsertOrganism at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+			
+			int id = Integer.parseInt(request.getParameter("id"));
+			
+				ServOrganism.delete(id);
+			
+		}
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
