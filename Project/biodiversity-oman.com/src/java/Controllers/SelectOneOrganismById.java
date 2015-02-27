3c554523953a4f0283503f0d0250a7a2ac4b28c8
Lenny <lenny.donnez@gmail.com>
2015-02-27 20:54:43 +0100

    Revert "Revert "SelectOneOrganismById""
    This reverts commit d887f97fa3d094932bdb155355d3c7180a9c87f6.

--- Project/biodiversity-oman.com/src/java/Controllers/SelectOneOrganismById.java
+++ Project/biodiversity-oman.com/src/java/Controllers/SelectOneOrganismById.java
@@ -5,11 +5,8 @@
  */
 package Controllers;
 
-import Service.*;
-import java.io.*;
-import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,8 +17,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Tom
  */
-@WebServlet(name = "DeleteOrganism", urlPatterns = {"/DeleteOrganism"})
-public class DeleteOrganism extends HttpServlet {
+@WebServlet(name = "SelectOneOrganismById", urlPatterns = {"/SelectOneOrganismById"})
+public class SelectOneOrganismById extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,17 +32,18 @@ public class DeleteOrganism extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-			
-			int id = Integer.parseInt(request.getParameter("id"));
-                        try{
-                        ServOrganism.delete(id);
-                        }
-                        catch(Exception ex){
-                            Logger.getLogger(DeleteOrganism.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-		 	
-		
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SelectOneOrganismById</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SelectOneOrganismById at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
