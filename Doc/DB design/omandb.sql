dfcaffce090cd2d2bc6fc005748eb591c0e167f8
Oualid Yousfi <oualid.yousfi@gmail.com>
2015-03-06 23:02:06 +0100

    Food tabel wijziging

--- Doc/DB design/omandb.sql
+++ Doc/DB design/omandb.sql
@@ -70,10 +70,8 @@ INSERT INTO `family` (`family_id`, `family_name`, `family_description`, `world_i
 --
 
 CREATE TABLE IF NOT EXISTS `food` (
-  `food_id` int(11) NOT NULL AUTO_INCREMENT,
-  `eaten_by_organism_id` int(11) DEFAULT NULL,
-  `eating_organism_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`food_id`),
+  `eaten_by_organism_id` int(11) NOT NULL,
+  `eating_organism_id` int(11) NOT NULL,
   KEY `Eaten_Organism_ID` (`eaten_by_organism_id`),
   KEY `Eating_organism_ID` (`eating_organism_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
